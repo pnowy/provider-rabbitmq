44a90b88b87ae1d6d69509babb12f2651e57ef08
Przemek Nowak <przemek.nowak.pl@gmail.com>
2025-04-15 15:54:28 +0200

    feat: vhost unit tests

--- internal/controller/vhost/vhost_test.go
+++ internal/controller/vhost/vhost_test.go
@@ -18,6 +18,11 @@ package vhost
 
 import (
 	"context"
+	"github.com/crossplane/crossplane-runtime/pkg/logging"
+	rabbithole "github.com/michaelklishin/rabbit-hole/v3"
+	"github.com/pnowy/provider-rabbitmq/apis/core/v1alpha1"
+	"github.com/pnowy/provider-rabbitmq/internal/rabbitmqclient/fake"
+	"github.com/pnowy/provider-rabbitmq/internal/rabbitmqmeta"
 	"testing"
 
 	"github.com/pnowy/provider-rabbitmq/internal/rabbitmqclient"
@@ -27,6 +32,7 @@ import (
 	"github.com/crossplane/crossplane-runtime/pkg/reconciler/managed"
 	"github.com/crossplane/crossplane-runtime/pkg/resource"
 	"github.com/crossplane/crossplane-runtime/pkg/test"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
 // Unlike many Kubernetes projects Crossplane does not use third party testing
@@ -38,8 +44,14 @@ import (
 // https://github.com/crossplane/crossplane/blob/master/CONTRIBUTING.md#contributing-code
 
 func TestObserve(t *testing.T) {
+	vHostTestName := "example-vhost"
+	defaultQueueType := "classic"
+	defaultTracing := false
+	defaultDescription := "example-description"
+
 	type fields struct {
 		service *rabbitmqclient.RabbitMqService
+		logger  logging.Logger
 	}
 
 	type args struct {
@@ -58,12 +70,56 @@ func TestObserve(t *testing.T) {
 		args   args
 		want   want
 	}{
-		// TODO: Add test cases.
+		"Vhost exists and is up to date": {
+			reason: "We should return ResourceExists and ResourceUpToDate to true",
+			fields: fields{
+				service: &rabbitmqclient.RabbitMqService{
+					Rmqc: &fake.MockClient{
+						MockGetVhost: func(name string) (rec *rabbithole.VhostInfo, err error) {
+							rec = &rabbithole.VhostInfo{
+								Name:             vHostTestName,
+								DefaultQueueType: defaultQueueType,
+								Tracing:          defaultTracing,
+								Description:      defaultDescription,
+							}
+							return rec, nil
+						},
+					},
+				},
+				logger: &fake.MockLog{},
+			},
+			args: args{
+				mg: &v1alpha1.Vhost{
+					ObjectMeta: metav1.ObjectMeta{
+						Annotations: map[string]string{
+							rabbitmqmeta.AnnotationKeyCrossplaneManaged: "true",
+						},
+					},
+					Spec: v1alpha1.VhostSpec{
+						ForProvider: v1alpha1.VhostParameters{
+							HostName: &vHostTestName,
+							VhostSettings: &v1alpha1.VhostSettings{
+								DefaultQueueType: &defaultQueueType,
+								Tracing:          &defaultTracing,
+								Description:      &defaultDescription,
+							},
+						},
+					},
+				},
+			},
+			want: want{
+				o: managed.ExternalObservation{
+					ResourceExists:          true,
+					ResourceLateInitialized: false,
+					ResourceUpToDate:        true,
+				},
+			},
+		},
 	}
 
 	for name, tc := range cases {
 		t.Run(name, func(t *testing.T) {
-			e := external{service: tc.fields.service}
+			e := external{service: tc.fields.service, log: tc.fields.logger}
 			got, err := e.Observe(tc.args.ctx, tc.args.mg)
 			if diff := cmp.Diff(tc.want.err, err, test.EquateErrors()); diff != "" {
 				t.Errorf("\n%s\ne.Observe(...): -want error, +got error:\n%s\n", tc.reason, diff)
