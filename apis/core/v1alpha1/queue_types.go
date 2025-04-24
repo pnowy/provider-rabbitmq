9cd5d4cd6cd19fd3573d92da5215ca5701216077
Przemek Nowak <przemek.nowak.pl@gmail.com>
2025-04-24 13:42:45 +0200

    feat: add queue and exchange arguments

--- apis/core/v1alpha1/queue_types.go
+++ apis/core/v1alpha1/queue_types.go
@@ -40,6 +40,7 @@ type QueueParameters struct {
 // +kubebuilder:validation:XValidation:rule="!has(oldSelf.type) || self.type == oldSelf.type",message="Type is immutable once set"
 // +kubebuilder:validation:XValidation:rule="!has(oldSelf.durable) || self.durable == oldSelf.durable",message="Durable is immutable once set"
 // +kubebuilder:validation:XValidation:rule="!has(oldSelf.autoDelete) || self.autoDelete == oldSelf.autoDelete",message="AutoDelete is immutable once set"
+// +kubebuilder:validation:XValidation:rule="!has(oldSelf.arguments) || self.arguments == oldSelf.arguments",message="Arguments are immutable once set"
 type QueueSettings struct {
 	Type       *string           `json:"type,omitempty"`
 	Durable    *bool             `json:"durable,omitempty"`
